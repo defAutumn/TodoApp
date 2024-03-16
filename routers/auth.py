0dfe810b35f243058566b68cd7945375602a592a
defAutumn <chromweechannelusa@gmail.com>
2024-03-16 12:45:26 +0300

    Added Authentication

--- routers/auth.py
+++ routers/auth.py
@@ -6,10 +6,11 @@ from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
 from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
-bcrypt_context = CryptContext(schemes=['Argon2'], deprecated='auto')
+bcrypt_context = CryptContext(schemes=['argon2'], deprecated='auto')
 
 
 class CreateUserRequest(BaseModel):
@@ -19,6 +20,7 @@ class CreateUserRequest(BaseModel):
     last_name: str
     password: str
     role: str
+
 
 def get_db():
     db = SessionLocal()
@@ -31,8 +33,18 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
+
 @router.post('/auth/', status_code=status.HTTP_200_OK)
-async def create_user(create_user_request: CreateUserRequest):
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
         username=create_user_request.username,
@@ -42,4 +54,15 @@ async def create_user(create_user_request: CreateUserRequest):
         hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True
     )
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
+
+
+@router.post('/token')
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return "Failed Authentication"
+
+    return "Success authentication"
