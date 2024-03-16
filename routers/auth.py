e6785c66a00408b8dd723ab0313387398e797f44
defAutumn <chromweechannelusa@gmail.com>
2024-03-16 11:26:49 +0300

    Added CreateUserRequest

--- routers/auth.py
+++ routers/auth.py
@@ -1,9 +1,45 @@
-from fastapi import APIRouter
-
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from models import Users
+from passlib.context import CryptContext
+from starlette import status
 
 router = APIRouter()
 
+bcrypt_context = CryptContext(schemes=['Argon2'], deprecated='auto')
 
-@router.get('/auth/')
-async def get_user():
-    return {'user': 'authenticated'}
+
+class CreateUserRequest(BaseModel):
+    email: str
+    username: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+def get_db():
+    db = SessionLocal()
+    print(type(db))
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.post('/auth/', status_code=status.HTTP_200_OK)
+async def create_user(create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        is_active=True
+    )
+    return create_user_model
