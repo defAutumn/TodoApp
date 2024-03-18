3c2bcad0a1898bdb69f2117961f548a20e51a531
Sergei <113532002+defAutumn@users.noreply.github.com>
2024-03-18 12:36:01 +0300

    Added Auth Get Todo

--- routers/todos.py
+++ routers/todos.py
@@ -33,12 +33,16 @@ class TodoRequest(BaseModel):
 
 @router.get('/', status_code=status.HTTP_200_OK)
 async def read_all(user: user_dependency,db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentiction Failed')
     return db.query(Todos).filter(Todos.owner == user.get('id')).all()
 
 
 @router.get('/todo/{todo_id}', status_code=status.HTTP_200_OK)
-async def get_todo(db: db_dependency, todo_id: int = Path(gt=0)):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+async def get_todo(user: user_dependency,db: db_dependency, todo_id: int = Path(gt=0)):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentiction Failed')
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner == user.get('id')).first()
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail='Todo not found.')
