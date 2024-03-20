43ccd29ef3da6d51ad9a74432798425c0085db40
Sergei <113532002+defAutumn@users.noreply.github.com>
2024-03-20 13:01:40 +0300

    Added Admin Router

---
+++ routers/admin.py
@@ -0,0 +1,27 @@
+from typing import Annotated
+from sqlalchemy.orm import Session
+from starlette import status
+from models import Todos
+from pydantic import BaseModel, Field
+from fastapi import APIRouter, Depends, HTTPException, Path
+from database import SessionLocal
+from .auth import get_current_user
+
+
+router = APIRouter(
+    prefix='/admin',
+    tags=['admin']
+)
+
+
+def get_db():
+    db = SessionLocal()
+    print(type(db))
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
