e6dfcaf9446620b53d24419ef0034ec9cc342173
Sergei <113532002+defAutumn@users.noreply.github.com>
2024-03-20 13:09:44 +0300

    Added Admin Delete endpoint

--- routers/admin.py
+++ routers/admin.py
@@ -26,8 +26,20 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
+
 @router.get('/todo', status_code=status.HTTP_200_OK)
 async def read_all(user: user_dependency, db: db_dependency):
     if user is None or user.get('role').lower() != 'admin':
-        raise HTTPException(status_code=401, detail='Authentication Failel.')
+        raise HTTPException(status_code=401, detail='Authentication Failed.')
     return db.query(Todos).all()
+
+
+@router.delete('/todo/{todo_id}', status_code=status.HTTP_200_OK)
+async def delet_todo(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
+    if user is None or user.get('role').lower() != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed.')
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=401, detail='Todo not found.')
+    db.query(Todos).filter(Todos.id == todo_id).delete()
+    db.commit()
